34f13cc2e7f254ac5301c70f5dc8113eeb623527
ViniciusAPortela <vinicius.portela.stm@gmail.com>
2019-10-10 00:26:07 -0300

    Added the Base of MDReader Class

--- src/services/MDReader.js
+++ src/services/MDReader.js
@@ -1,28 +1,37 @@
-/*
-    MARKDOWN READER
-    Reads and Render the MD files of Wiki Pages, also reads the additional stuff 
-    that isn't in default markdown language
-*/
+/**
+ * MARKDOWN READER
+ * Reads and Render the MD files of Wiki Pages, also reads the additional stuff 
+ * that isn't in default markdown language
+ * 
+ * This class Transform everything in a readable array for the PageRender React Component
+ */
 
-export default new class MDReader{
+//Reads the file, static file (For now)
+const readline = require('readline')
+const fs = require('fs');
 
-  /*
-    Reads the page
-  */
-  read(page){
-    let file = (process.env.PUBLIC_URL + `./pages/${page}/ukuu.png`);
-    // ... Code
+class MDReader {
+  read(file){
+    const data = fs.createReadStream(file);
+
+    const readInterface = readline.createInterface({
+      input: data,
+      console: false,
+    });
+
+    readInterface.on('line', function(line) {
+      console.log(line);
+    });
   }
 
-  /*
-    Generate The JSX Structure that will be read by the PageRender
-  */
-  render(page){
-    if(typeof page === undefined) throw `${page} not exists`;
-
-    this.read(page);
-
-    return(null);
-  }
+  //TODO: Create the MD to Array Converter
+  toArray(){}
 
 }
+
+/* TESTING AREA */
+const reader = new MDReader;
+reader.read('article.md');
+/* TESTING AREA */
+
+module.exports = MDReader;
